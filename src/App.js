ddda46cb52008ce1f596a025e91b33444532e58b
Nick Correa <nickcorreagarcia@gmail.com>
2021-07-23 16:31:36 -0500

    Update App Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123",
+    email: "nicoga@mail.com",
+    password: "nico123",
   };
 
   const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
 
-  const Logut = () => {
-    console.log("Logout");
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not match");
+      setError("Details do not match");
+    }
   };
 
-  return <div className="App">Hello</div>;
+  const Logout = () => {
+    console.log("Logout");
+    setUser({ name: "", email: "" });
+  };
+
+  return (
+    <div className="App">
+      {user.email !== "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  );
 }
 
 export default App;
